12ab0723bd0c14a34af0fa1132146d1bfad2a41a
NhatComNhom <nhatcomnhom@gmail.com>
2023-05-05 20:54:25 +0900

    add some admin features

--- app/admin/admin.php
+++ app/admin/admin.php
@@ -14,81 +14,102 @@ include_once('./check_attendance.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin Dashboard</title>
-  <link rel="stylesheet" href="../css/reset.css">
-  <link rel="stylesheet" href="../css/style.css">
-  <link href="../css/bootstrap.min.css" rel="stylesheet">
-  <link href="../css/datatables.min.css" rel="stylesheet">
-  <script src="../js/bootstrap.bundle.min.js"></script>
-  <script src="../js/jquery-3.6.4.min.js"></script>
-  <script src="../js/datatables.min.js"></script>
-  <script src="../js/pdfmake.min.js"></script>
-  <script src="../js/vfs_fonts.js"></script>
-  <script src="../js/custom.js"></script>
-  <title>LSI勤怠管理ADMIN</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/datatables.min.css" rel="stylesheet">
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery-3.6.4.min.js"></script>
+    <script src="../js/datatables.min.js"></script>
+    <script src="../js/pdfmake.min.js"></script>
+    <script src="../js/vfs_fonts.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable({
+                dom: 'Bfrtip',
+                buttons: ['csv', 'excel', 'pdf', 'print'],
+                searching: false, // tắt tính năng search
+                lengthChange: false, // tắt tính năng show entries
+                pageLength: 10 // thiết lập số entries mặc định là 10
+            });
+        });
+
+    </script>
+    <title>LSI勤怠管理ADMIN</title>
 </head>
 <body>
 <header>
-  <nav class="navbar navbar-expand-sm">
-      <div class="container d-flex">
-          <a class="navbar-brand" href="#">
-              <h1 class="text-light">
-                  LSI勤怠管理_ADMINページ
-              </h1>
-          </a>
-          <div class="collapse navbar-collapse justify-content-end" id="navBarHeader">
-              <ul class="navbar-nav ">
-                  <?php
-                      if (isset($_SESSION["username"])){
-                          echo "<li class='nav-item'><p class='text-info mt-2 ml-1'>Hello there, ".$_SESSION["name"]."</p></li>";
-                          echo "<li class='nav-item'><a href='../include/logout.inc.php' class='nav-link text-light'>ログアウト</a></li>";
-                      } else  {
-                          echo "<li class='nav-item'><a href='../login.php' class='nav-link text-light'>ログイン</a></li>";
-                      }
-                  ?>
-              </ul>
-          </div>
-      </div>
-  </nav>
-  </header>
-  <main>
-        <h2 class="text-light">
-          this is admin page!!
-        </h2>
-        <h1><?php echo $employee_name ?> - <?php echo $month ?>/<?php echo $year ?></h1>
-        <div class="container">
-            <div class="data_table">
-                <table id="example" class="table table-striped table-bordered table-primary" style="width:100%">
-                    <thead class="bg-light">
-                        <tr class="table-primary">
-                        <th scope="col">日付曜日</th>
-                        <th scope="col">出勤</th>
-                        <th scope="col">lunch開始</th>
-                        <th scope="col">lunch終了</th>
-                        <th scope="col">退勤</th>
-                        <th scope="col">会社/在宅</th>
-                        <th scope="col">備考欄</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php while ($row = pg_fetch_assoc($result)): ?>
-                        <tr>
-                        <td><?php echo $row['day'] ?></td>
-                        <td><?php echo $row['check_in'] ?></td>
-                        <td><?php echo $row['start_break'] ?></td>
-                        <td><?php echo $row['end_break'] ?></td>
-                        <td><?php echo $row['check_out'] ?></td>
-                        <td><?php echo $row['remote']==1?'会社':'在宅' ?></td>
-                        <td></td>
-                        </tr>
-                        <?php endwhile; ?>
-                    </tbody>
-                </table>
+    <nav class="navbar navbar-expand-sm">
+        <div class="container d-flex">
+            <a class="navbar-brand" href="#">
+                <h1 class="text-light">
+                    LSI勤怠管理_ADMINページ
+                </h1>
+            </a>
+            <div class="collapse navbar-collapse justify-content-end" id="navBarHeader">
+                <ul class="navbar-nav ">
+                    <li class='nav-item'><p class='text-info mt-2 ml-1'>Hello there, ADMIN</p></li>";
+                    <li class='nav-item'><a href='../include/logout.inc.php' class='nav-link text-light'>ログアウト</a></li>";
+                </ul>
             </div>
         </div>
+    </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row mt-3 mb-3">
+                <div class="col-md-4">
+                    <input type="text" class="form-control" id="employee_name" name="employee_name" placeholder="社員の名前">
+                </div>
+                <div class="col-md-3">
+                    <select class="form-control" id="month" name="month">
+                        <option value="" disabled selected>月</option>
+                    <?php for ($i=1; $i<=12; $i++) {
+                        //$selected = ($i == $month) ? 'selected' : '';
+                        $selected = $i;
+                        printf('<option value="%02d" %s>%02d</option>', $i, $selected, $i);
+                    } ?>
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <select class="form-control" id="year" name="year">
+                    <option value="" disabled selected>年</option>
+                    <?php for ($i=date("Y"); $i>=2010; $i--) {
+                        //$selected = ($i == $year) ? 'selected' : '';
+                        $selected = $i;
+                        printf('<option value="%d" %s>%d</option>', $i, $selected, $i);
+                    } ?>
+                    </select>
+                </div>
+                <div class="col-md-2 justify-content-right align-self-end">
+                    <button type="submit" id="check" class="btn btn-primary" name="check">検索</button>
+                </div>
+            </div>
+            <div id="table_result">
+                <div class="data_table">
+                    <h1 class="justify-content-center" style="display: flex; align-items: center; justify-content: center;"><?php echo $employee_name ?> - <?php echo $month ?>/<?php echo $year ?></h1>
+                    <table id="example" class="table table-striped table-bordered table-primary" style="width:100%">
+                        <thead class="bg-light">
+                            <tr class="table-primary">
+                            <th scope="col">日付曜日</th>
+                            <th scope="col">出勤</th>
+                            <th scope="col">lunch開始</th>
+                            <th scope="col">lunch終了</th>
+                            <th scope="col">退勤</th>
+                            <th scope="col">会社/在宅</th>
+                            <th scope="col">備考欄</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
   </main>
   <?php 
       include_once('../view/footer.php'); 
--- app/admin/check_attendance.php
+++ app/admin/check_attendance.php
@@ -7,27 +7,28 @@
     $year = 2023;
 
     // Tạo câu truy vấn SQL
-    $sql = "
-        SELECT
-            tbl_employees.name,
-            to_char(check_time, 'DD') AS day,
-            MAX(CASE WHEN action = 'check_in' THEN check_time::time END) AS check_in,
-            MAX(CASE WHEN action = 'start_break' THEN check_time::time END) AS start_break,
-            MAX(CASE WHEN action = 'end_break' THEN check_time::time END) AS end_break,
-            MAX(CASE WHEN action = 'check_out' THEN check_time::time END) AS check_out,
-            MAX(CASE WHEN action = 'check_in' THEN CASE WHEN remote = true THEN 1 ELSE 0 END END) AS remote 
-        FROM tbl_checkinout
-        INNER JOIN tbl_employees ON tbl_checkinout.user_id = tbl_employees.id
-        WHERE tbl_employees.name = '$employee_name'
-            AND EXTRACT(MONTH FROM check_time) = $month
-            AND EXTRACT(YEAR FROM check_time) = $year
-        GROUP BY tbl_employees.id, to_char(check_time, 'DD')
-        ORDER BY to_char(check_time, 'DD') ASC;
-    ";
+    if(isset($employee_name) && isset($month) && isset($year)){
+        $sql = "
+            SELECT
+                tbl_employees.name,
+                to_char(check_time, 'DD') AS day,
+                MAX(CASE WHEN action = 'check_in' THEN check_time::time END) AS check_in,
+                MAX(CASE WHEN action = 'start_break' THEN check_time::time END) AS start_break,
+                MAX(CASE WHEN action = 'end_break' THEN check_time::time END) AS end_break,
+                MAX(CASE WHEN action = 'check_out' THEN check_time::time END) AS check_out,
+                MAX(CASE WHEN action = 'check_in' THEN CASE WHEN remote = true THEN 1 ELSE 0 END END) AS remote 
+            FROM tbl_checkinout
+            INNER JOIN tbl_employees ON tbl_checkinout.user_id = tbl_employees.id
+            WHERE tbl_employees.name = '$employee_name'
+                AND EXTRACT(MONTH FROM check_time) = $month
+                AND EXTRACT(YEAR FROM check_time) = $year
+            GROUP BY tbl_employees.id, to_char(check_time, 'DD')
+            ORDER BY to_char(check_time, 'DD') ASC;
+        ";
 
-    // Thực hiện truy vấn SQL
-    $result = pg_query($conn, $sql);
-
+        // Thực hiện truy vấn SQL
+        $result = pg_query($conn, $sql);
+    }
     pg_close($conn);
 ?>
 
