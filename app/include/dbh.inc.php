bc652d7f73b1120a5e07da8be8bf78008b7f8261
nhatcomnhom <nhatcomnhom@gmail.com>
2024-02-14 23:20:15 +0900

    fix connect database on local

--- app/include/dbh.inc.php
+++ app/include/dbh.inc.php
@@ -1,10 +1,17 @@
 <?php
 //render-server
-$host = "dpg-ch2fv8l269v61fd6r56g-a.singapore-postgres.render.com";
+// $host = "dpg-ch2fv8l269v61fd6r56g-a.singapore-postgres.render.com";
+// $port = "5432";
+// $dbname = "lsi_kintaikanri";
+// $user = "lsi_kintaikanri";
+// $password = "4ET9Ws3M43C3xsr5VREqp0pW6WMJQATM";
+
+$host = "my-postgres-container";
 $port = "5432";
 $dbname = "lsi_kintaikanri";
-$user = "lsi_kintaikanri";
-$password = "4ET9Ws3M43C3xsr5VREqp0pW6WMJQATM";
+$user = "lsi";
+$password = "lsilsilsi";
+
 
 // Create connection
 $conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password);
